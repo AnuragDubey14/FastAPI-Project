4c157f07662011a157edc37b80b79e54ec81e488
Anurag Dubey <129617718+AnuragDubey14@users.noreply.github.com>
2025-10-04 17:13:06 +0000

    type hinting

---
+++ type_hints.py
@@ -0,0 +1,33 @@
+from typing import Any
+
+class City:
+    def __init__(self,name,location):
+        self.name=name
+        self.location=location
+
+
+text:str="value"
+pert:int = 90
+temp:float=37.5
+
+
+number : int|float=12 
+digits:list[int]=[1,2,3,4,5]
+table_5:tuple[int,...]=(5,10,15,20,25)
+
+hampshire=City("hamspshire",2048593)
+
+city_temp:tuple[City,float]=(hampshire,20.5)
+
+shipment:dict[str,Any]={
+    "id":12345,
+    "weight":1.2,
+    "content":"Wooden table",
+    "status":"in transit"
+}
+
+
+def root(num:int | float, exp:float|None =0.5) -> float:
+    return pow(num,.5)
+
+root_25=root(25)
